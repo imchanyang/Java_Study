62bbf6c681c57df4d7ae30695af1ee4f8d18b9cf
imchanyang <cksdid636@naver.com>
2022-03-01 17:44:37 +0900

    Calendar2

--- Calendar/src/imchanyang/calendar/Calendar.java
+++ Calendar/src/imchanyang/calendar/Calendar.java
@@ -3,7 +3,13 @@ package imchanyang.calendar;
 import java.util.Scanner;
 
 public class Calendar {
-    public static void main(String[] args) {
+    private static final int[] maxDays = {31, 29, 31, 30, 31, 30, 31, 30, 31, 30, 31, 30};
+
+    public int getMaxDaysOfMonth(int month) {
+        return maxDays[month-1];
+    }
+
+    public void printSampleCalendar() {
         // println을 활용해서 달력 출력
         System.out.println(" 일 월 화 수 목 금 토");
         System.out.println("--------------------");
@@ -11,18 +17,56 @@ public class Calendar {
         System.out.println(" 8  9 10 11 12 13 14");
         System.out.println("15 16 17 18 19 20 21");
         System.out.println("22 23 24 25 26 27 28");
+    }
+
+    public static void main(String[] args) {
 
         // 숫자를 입력받아 해당하는 달의 최대 일수를 출력하는 프로그램
         Scanner scanner = new Scanner(System.in);
         System.out.println("달을 입력하세요 : ");
         int month = scanner.nextInt();
 
-        int[] maxDays = {31, 28, 31, 30, 31, 30, 31, 30, 31, 30, 31, 30};
+        Calendar cal = new Calendar();
 
-        System.out.printf("%d월은 %d일까지 있습니다.\n", month, maxDays[month-1]);
+
+        System.out.printf("%d월은 %d일까지 있습니다.\n", month, cal.getMaxDaysOfMonth(month));
         scanner.close();
 
 
+        System.out.println("------------------if 조건문을 이용해서 출력---------------");
+
+        // if를 이용해서 출력
+        if(month == 2) {
+            System.out.printf("%d월은 29일까지 있습니다.\n", month);
+        }
+        else if(month % 2 == 0) {
+            System.out.printf("%d월은 30일까지 있습니다.\n", month);
+        }
+        else {
+            System.out.printf("%d월은 31일까지 있습니다.\n", month);
+        }
+
+        System.out.println("------------------switch 조건문을 이용해서 출력---------------");
+
+        switch (month) {
+            case 2:
+                System.out.printf("%d월은 29일까지 있습니다.\n", month);
+                break;
+            case 4:
+            case 6:
+            case 8:
+            case 10:
+            case 12:
+                System.out.printf("%d월은 30일까지 있습니다.\n", month);
+                break;
+            default:
+                System.out.printf("%d월은 31일까지 있습니다.\n", month);
+                break;
+
+        }
+
+        System.out.println("------------------예시 달력---------------");
+        cal.printSampleCalendar();
 
 
     }
