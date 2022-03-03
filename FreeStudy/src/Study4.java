f396dea73e8b3ac0c3b26a9d820cdfa486bc17c6
imchanyang <cksdid636@naver.com>
2022-03-04 02:51:38 +0900

    Study4

---
+++ FreeStudy/src/Study4.java
@@ -0,0 +1,14 @@
+
+import java.util.ArrayList;
+
+public class Study4 {
+    public static void main(String[] args) {
+        ArrayList al = new ArrayList();
+        al.add(1);
+        al.add(2);
+
+
+        System.out.println(al.get(0));
+        System.out.println(((int) al.get(0) + 1));
+    }
+}
