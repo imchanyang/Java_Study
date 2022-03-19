969019b60d48f54978ed9fe2a901aed6fdbbc340
imchanyang <cksdid636@naver.com>
2022-03-19 15:12:31 +0900

    Java의 정석 start

--- FreeStudy/src/Study5.java
+++ FreeStudy/src/Study5.java
@@ -1,2 +1,32 @@
-package PACKAGE_NAME;public class Study5 {
+interface t {
+    public void add1(int a, int b);
+}
+class a implements t{
+    int a;
+    int b;
+
+    public void add(int a, int b) {
+        System.out.println(a + b);
+    }
+
+    public void add(int a, int b, int c) {
+        System.out.println(a + b);
+    }
+
+    public void add1(int a, int b) {
+        System.out.println("complete");
+    }
+
+
+}
+
+
+public class Study5 {
+    public static void main(String[] args) {
+        a a_ = new a();
+
+
+        System.out.println(b_.print());
+
+    }
 }
