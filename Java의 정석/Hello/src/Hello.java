5483f55e598c3551f16aae5822865268c67891a1
imchanyang <cksdid636@naver.com>
2022-03-19 15:11:08 +0900

    Java의 정석 start

---
+++ Java의 정석/Hello/src/Hello.java
@@ -0,0 +1,12 @@
+/*
+Date   : 2022/03/19
+Author : Imchanyang
+Email : cksdid636@naver.com
+*/
+public class Hello {
+    // main 메서드의 선언부
+    // java.exe에 의해 호출된다.
+    public static void main(String[] args) {
+        System.out.println("Hello, World.");
+    }
+}
